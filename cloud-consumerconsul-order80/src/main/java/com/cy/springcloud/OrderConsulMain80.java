da5cce0d60ee413de9c9d0d81af30f27cc2fa1a1
TYHuohuo <1031996692@qq.com>
2021-05-31 08:52:00 +0800

    conslue注册中心配置

---
+++ cloud-consumerconsul-order80/src/main/java/com/cy/springcloud/OrderConsulMain80.java
@@ -0,0 +1,17 @@
+package com.cy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author huohuo
+ * @create: 2021/5/30 16:12
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class, args);
+    }
+}
---
+++ cloud-poviderconsul-payment8006/src/main/java/com/cy/springcloud/PaymentConsulMain8006.java
@@ -0,0 +1,17 @@
+package com.cy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author huohuo
+ * @create: 2021/5/30 15:52
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentConsulMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentConsulMain8006.class, args);
+    }
+}
